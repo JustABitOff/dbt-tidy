9c8c1c7d2da12ec4a4222f6d88ca5221525c1da6
JustABitOff <cameronjcyr@gmail.com>
2025-02-26 17:33:43 -0600

    adds v10 support

---
+++ tidy/manifest/v10/documentation/documentation.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from pydantic import BaseModel, ConfigDict
+
+from tidy.manifest.v10.bases.enums import ResourceType
+
+
+class Documentation(BaseModel):
+    model_config = ConfigDict(
+        extra='forbid',
+    )
+    name: str
+    resource_type: ResourceType
+    package_name: str
+    path: str
+    original_file_path: str
+    unique_id: str
+    block_contents: str

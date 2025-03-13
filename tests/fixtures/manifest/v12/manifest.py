c74d6987e16b857af1242f98833ec12801d72c64
JustABitOff <cameronjcyr@gmail.com>
2025-03-13 17:48:51 -0500

    multi source tests

--- tests/fixtures/manifest/v12/manifest.py
+++ tests/fixtures/manifest/v12/manifest.py
@@ -234,8 +234,60 @@ def manifestv12_fixture():
         groups={},
         selectors={},
         disabled=None,
-        parent_map={},
-        child_map={},
+        parent_map={
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": [
+                f"source.{PACKAGE_NAME}.source_one.table_one",
+            ],
+            f"model.{PACKAGE_NAME}.stg_source_two__table_two": [
+                f"source.{PACKAGE_NAME}.source_two.table_two",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_two__table_two": [
+                f"model.{PACKAGE_NAME}.stg_source_two__table_two",
+            ],
+            f"model.{PACKAGE_NAME}.dim_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.fct_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.dim_two": [
+                f"model.{PACKAGE_NAME}.int_source_two__table_two",
+            ],
+            f"model.{PACKAGE_NAME}.fct_two": [
+                f"model.{PACKAGE_NAME}.int_source_two__table_two",
+            ],
+            f"source.{PACKAGE_NAME}.source_one.table_one": [],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [],
+        },
+        child_map={
+            f"source.{PACKAGE_NAME}.source_one.table_one": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+            ],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [
+                f"model.{PACKAGE_NAME}.stg_source_two__table_two",
+            ],
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.stg_source_two__table_two": [
+                f"model.{PACKAGE_NAME}.int_source_two__table_two",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.dim_one",
+                f"model.{PACKAGE_NAME}.fct_one",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_two__table_two": [
+                f"model.{PACKAGE_NAME}.dim_two",
+                f"model.{PACKAGE_NAME}.fct_two",
+            ],
+            f"model.{PACKAGE_NAME}.dim_one": [],
+            f"model.{PACKAGE_NAME}.fct_one": [],
+            f"model.{PACKAGE_NAME}.dim_two": [],
+            f"model.{PACKAGE_NAME}.fct_two": [],
+        },
         group_map={},
         semantic_models={},
         saved_queries={},
@@ -383,8 +435,40 @@ def manifestv12_direct_join_to_source_fixture():
         groups={},
         selectors={},
         disabled=None,
-        parent_map={},
-        child_map={},
+        parent_map={
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": [
+                f"source.{PACKAGE_NAME}.source_one.table_one",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+                f"source.{PACKAGE_NAME}.source_two.table_two",
+            ],
+            f"model.{PACKAGE_NAME}.dim_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.fct_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"source.{PACKAGE_NAME}.source_one.table_one": [],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [],
+        },
+        child_map={
+            f"source.{PACKAGE_NAME}.source_one.table_one": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+            ],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.dim_one",
+                f"model.{PACKAGE_NAME}.fct_one",
+            ],
+            f"model.{PACKAGE_NAME}.dim_one": [],
+            f"model.{PACKAGE_NAME}.fct_one": [],
+        },
         group_map={},
         semantic_models={},
         saved_queries={},
@@ -446,8 +530,14 @@ def manifestv12_duplicate_sources_fixture():
         groups={},
         selectors={},
         disabled=None,
-        parent_map={},
-        child_map={},
+        parent_map={
+            f"source.{PACKAGE_NAME}.source_one.table_one": [],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [],
+        },
+        child_map={
+            f"source.{PACKAGE_NAME}.source_one.table_one": [],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [],
+        },
         group_map={},
         semantic_models={},
         saved_queries={},
@@ -809,6 +899,187 @@ def manifestv12_model_fanout_fixture():
             f"model.{PACKAGE_NAME}.fct_three": [],
             f"model.{PACKAGE_NAME}.fct_four": [],
             f"model.{PACKAGE_NAME}.fct_five": [],
+        },
+        group_map={},
+        semantic_models={},
+        saved_queries={},
+        unit_tests={},
+    )
+
+
+@pytest.fixture
+def manifestv12_multiple_sources_joined_fixture():
+    return ManifestV12(
+        metadata=ManifestMetadata(
+            project_name=PACKAGE_NAME,
+        ),
+        nodes={
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": Model(
+                database="unit_test_db",
+                schema="staging",
+                name="stg_source_one__table_one",
+                resource_type="model",
+                package_name=PACKAGE_NAME,
+                path="",
+                original_file_path="",
+                unique_id=f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+                fqn=[
+                    PACKAGE_NAME,
+                    "stg_source_one__table_one",
+                ],
+                alias="stg_source_one__table_one",
+                checksum=FileHash(name="", checksum=""),
+                depends_on=DependsOn(
+                    nodes=[
+                        f"source.{PACKAGE_NAME}.source_one.table_one",
+                        f"source.{PACKAGE_NAME}.source_two.table_two",
+                    ]
+                ),
+            ),
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": Model(
+                database="unit_test_db",
+                schema="intermediate",
+                name="int_source_one__table_one",
+                resource_type="model",
+                package_name=PACKAGE_NAME,
+                path="",
+                original_file_path="",
+                unique_id=f"model.{PACKAGE_NAME}.int_source_one__table_one",
+                fqn=[
+                    PACKAGE_NAME,
+                    "int_source_one__table_one",
+                ],
+                alias="int_source_one__table_one",
+                checksum=FileHash(name="", checksum=""),
+                depends_on=DependsOn(
+                    nodes=[
+                        f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+                    ]
+                ),
+            ),
+            f"model.{PACKAGE_NAME}.dim_one": Model(
+                database="unit_test_db",
+                schema="marts",
+                name="dim_one",
+                resource_type="model",
+                package_name=PACKAGE_NAME,
+                path="",
+                original_file_path="",
+                unique_id=f"model.{PACKAGE_NAME}.dim_one",
+                fqn=[
+                    PACKAGE_NAME,
+                    "dim_one",
+                ],
+                alias="dim_one",
+                checksum=FileHash(name="", checksum=""),
+                depends_on=DependsOn(
+                    nodes=[
+                        f"model.{PACKAGE_NAME}.int_source_one__table_one",
+                    ]
+                ),
+            ),
+            f"model.{PACKAGE_NAME}.fct_one": Model(
+                database="unit_test_db",
+                schema="marts",
+                name="fct_one",
+                resource_type="model",
+                package_name=PACKAGE_NAME,
+                path="",
+                original_file_path="",
+                unique_id=f"model.{PACKAGE_NAME}.fct_one",
+                fqn=[
+                    PACKAGE_NAME,
+                    "fct_one",
+                ],
+                alias="fct_one",
+                checksum=FileHash(name="", checksum=""),
+                depends_on=DependsOn(
+                    nodes=[
+                        f"model.{PACKAGE_NAME}.int_source_one__table_one",
+                    ]
+                ),
+            ),
+        },
+        sources={
+            f"source.{PACKAGE_NAME}.source_one.table_one": SourceDefinition(
+                database="unit_test_db",
+                schema="source_one",
+                name="table_one",
+                resource_type="source",
+                package_name=PACKAGE_NAME,
+                path="",
+                original_file_path="",
+                unique_id=f"source.{PACKAGE_NAME}.source_one.table_one",
+                fqn=[
+                    PACKAGE_NAME,
+                    "source_one",
+                    "table_one",
+                ],
+                source_name="source_one",
+                source_description="",
+                loader="",
+                identifier="",
+            ),
+            f"source.{PACKAGE_NAME}.source_two.table_two": SourceDefinition(
+                database="unit_test_db",
+                schema="source_two",
+                name="table_two",
+                resource_type="source",
+                package_name=PACKAGE_NAME,
+                path="",
+                original_file_path="",
+                unique_id=f"source.{PACKAGE_NAME}.source_two.table_two",
+                fqn=[
+                    PACKAGE_NAME,
+                    "source_two",
+                    "table_two",
+                ],
+                source_name="source_two",
+                source_description="",
+                loader="",
+                identifier="",
+            ),
+        },
+        macros={},
+        docs={},
+        exposures={},
+        metrics={},
+        groups={},
+        selectors={},
+        disabled=None,
+        parent_map={
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": [
+                f"source.{PACKAGE_NAME}.source_one.table_one",
+                f"source.{PACKAGE_NAME}.source_two.table_two",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.dim_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.fct_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"source.{PACKAGE_NAME}.source_one.table_one": [],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [],
+        },
+        child_map={
+            f"source.{PACKAGE_NAME}.source_one.table_one": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+            ],
+            f"source.{PACKAGE_NAME}.source_two.table_two": [
+                f"model.{PACKAGE_NAME}.stg_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.stg_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.int_source_one__table_one",
+            ],
+            f"model.{PACKAGE_NAME}.int_source_one__table_one": [
+                f"model.{PACKAGE_NAME}.dim_one",
+                f"model.{PACKAGE_NAME}.fct_one",
+            ],
+            f"model.{PACKAGE_NAME}.dim_one": [],
+            f"model.{PACKAGE_NAME}.fct_one": [],
         },
         group_map={},
         semantic_models={},
